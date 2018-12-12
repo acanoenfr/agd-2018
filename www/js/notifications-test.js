82ffc96b189955ce78e8a38c3eb793b5d245f7ee
Julien Bruguet <lineproex@gmail.com>
2018-12-12 11:09:13 +0100

    Restructuration de notifications-test.js + ajout de commentaires
    Pour mieux comprendre comme ça fonctionne.

--- www/js/notifications-test.js
+++ www/js/notifications-test.js
@@ -3,7 +3,18 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
 	// Fonction servant au déploiement de notifications
     cordova.plugins.notification.local.schedule([
-        { id: 0, title: 'Bonjour', text: 'Je suis un test', foreground: true }, // Notification qui s'envoie au lancement de l'appli
-        { id: 1, title: '10sec', text: 'Salut', trigger: { in : 10, unit: 'second' } } // Notification qui s'envoie 10sec après le lancement de l'appli
+        { id: 0, // Notification qui s'envoie au lancement de l'appli
+        	title: 'Devoirs à rendre', // Titre
+        	text: 'Pour les TP1A', // Description
+        	foreground: true,
+        	vibrate: true }, // La notification fera vibrer le téléphone (car true)
+        { id: 1, // Notification qui s'envoie 10sec après le lancement de l'appli
+        	title: "Réunion d'informations",
+        	text: 'De 10h à 11h',
+        	trigger: { in : 10, unit: 'second' }, // S'envoie dans 10 secondes
+        										/* Il est également possible de l'envoyer à une date précise avec: 	*/
+        										/* trigger: { at: new Date(2017, 10, 27, 15, 30) }	*	*	*	*	*/
+        										/* Format année, mois, jour, heure, minute			*	*	*	*	*/
+        	vibrate: true }
     ]);
 }
