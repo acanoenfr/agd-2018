52364ad7dbc543ae5bc99903c71672509cd1985f
Alexandre Canoën <contact@acanoen.fr>
2019-01-08 13:00:00 +0100

    Add the database connection
    Modify index.html and index.js

--- www/js/index.js
+++ www/js/index.js
@@ -13,20 +13,25 @@ let app = {
         showEvents()
         document.getElementById('previous').addEventListener('click', showEvents)
         document.getElementById('next').addEventListener('click', showEvents)
-        let myDB = window.sqlitePlugin.openDatabase({
-            name: "agd-2018.db",
-            location: "default",
-            androidDatabaseProvider: "system"
+        let myDB = window.openDatabase("Events0000000", "1.0", "All Deadlines", 2000000)
+        myDB.transaction(function (txn) {
+            txn.executeSql("CREATE TABLE IF NOT EXISTS events (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, content TEXT, color TEXT NOT NULL, start TEXT NOT NULL, end TEXT)", [], function (tx, res) {
+                console.info('Create table events')
+            }, function (tx, err) {
+                console.warn('Err[' + err.code + ']: ' + err.message)
+            })
+            /* txn.executeSql(`INSERT INTO events(title, color, start) VALUES(?, ?, ?)`, ['Event', '#0000ff', '2019-1-15'], function (tx, res) {
+                console.info('Insert data in events')
+            }, function (tx, err) {
+                console.warn('Err[' + err.code + ']: ' + err.message)
+            }); */
         })
         myDB.transaction(function (txn) {
-            txn.executeSql("CREATE TABLE IF NOT EXISTS events (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, content TEXT, color TEXT NOT NULL, start TEXT NOT NULL, end TEXT)")
-        })
-        myDB.transaction(function (txn) {
-           txn.executeSql('SELECT * FROM events', [], function (tx, res) {
-               console.log(res.rows)
-           }, function (tx, err) {
-               console.warn(err)
-           })
+            txn.executeSql("SELECT * FROM events", [], function (tx, res) {
+                console.info(res.rows)
+            }, function (tx, err) {
+                console.warn('Err[' + err.code + ']: ' + err.message)
+            })
         })
     }
 }
