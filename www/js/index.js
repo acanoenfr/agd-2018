df0b2074c48053482902f9dfed08dea3bcfea03b
Julien Bruguet <lineproex@gmail.com>
2019-01-09 14:26:33 +0100

    Script pour modification des deadlines partie 1
    Pour l'instant je cherche à afficher les deadlines correctement dans la console

--- www/js/index.js
+++ www/js/index.js
@@ -23,8 +23,17 @@ let app = {
                 txn.executeSql("SELECT * FROM events", [], function (tx, res) {
                     console.info(res.rows)
                     let events = res.rows
+
+                    let checkListener; // Pour vérifier si il y a déjà un event listener sur la date
+                    let dateCheck; // Stocke la date de la case cliquée pour la requête SQL affichant les deadlines du jour
+                    let dateTemp; // Deadline tampon pour vérifier si il y a déjà une deadline sur la date actuelle (Pour l'event listener)
+
                     for (let i = 0; i < res.rows.length; i++) {
                         date = events[i].start.split('-')
+
+                        dateTemp = events[i].start; // Mise en tampon de la date en cours
+                        if(dateTemp != dateCheck) checkListener++; // Incrémente checkListener de 1 si il on est déjà passé sur cette date
+
                         if (currentMonth == date[1] - 1 && currentYear == date[0]) {
                             let elementStart = document.getElementById(events[i].start)
                             let elementEnd = document.getElementById(events[i].end)
@@ -34,9 +43,30 @@ let app = {
                                 icon.setAttribute('class', 'fas fa-star')
                                 icon.setAttribute('style', `color: ${events[i].color}; font-size: 1rem;`)
                                 elementStart.appendChild(icon)
-                                elementStart.addEventListener('click', function(){
-                                    window.location = 'modSupDeadline.html'
-                                })
+
+                                if (checkListener == 0) {
+                                    checkListener++;
+
+                                    elementStart.addEventListener('click', function(){
+
+                                        /* Tout ça sert à afficher les deadlines dans la console */
+                                        dateCheck = events[i].start;
+                                        myDB.transaction(function (txn2) {
+                                            txn2.executeSql("SELECT * FROM events WHERE start=?", [dateCheck], function (tx2, res2) {
+                                                for (let k = 0; k < res2.rows.length; k++) {
+                                                    console.log(res2.rows[k].title);
+                                                    console.log(res2.rows[k].content);
+                                                    console.log(res2.rows[k].start);
+                                                    console.log(res2.rows[k].end);
+                                                }
+                                            }, function (tx2, err2) {
+                                                console.warn('Err[' + err.code + ']: ' + err.message);
+                                            });
+                                        });
+
+                                        //window.location = 'modSupDeadline.html'
+                                    })
+                                }
                             } else {
                                 elementStart.setAttribute('class', 'event')
                                 elementEnd.setAttribute('class', 'event')
