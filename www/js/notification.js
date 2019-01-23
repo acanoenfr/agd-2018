34364f7b80f0368db8ad7a060408622bc3f950fb
Alexandre Canoën <contact@acanoen.fr>
2019-01-23 09:16:47 +0100

    Create final file
    notification.js

---
+++ www/js/notification.js
@@ -0,0 +1,5 @@
+document.addEventListener("deviceready", onDeviceReady, false)
+
+function onDeviceReady() {
+    let myDB = window.openDatabase("Events00000000", "1.0", "All Deadlines", 2000000)
+}
