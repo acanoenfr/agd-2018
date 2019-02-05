2527a17fc6ff0b79359d29dca7e66a3315e525f8
Julien Bruguet <lineproex@gmail.com>
2019-02-05 11:23:16 +0100

    Re-style de la page d'accueil + début navbar

---
+++ www/js/navbar.js
@@ -0,0 +1,36 @@
+var accueilbtn = $("#nav-accueil");
+var historiquebtn = $("#nav-historique");
+var exporterbtn = $("#nav-exporter");
+
+accueilbtn.click(function() {
+	$("#nav-historique span").text("");
+	historiquebtn.removeClass("current");
+
+	$("#nav-exporter span").text("");
+	exporterbtn.removeClass("current");
+
+	$("#nav-accueil span").text("Accueil");
+	accueilbtn.addClass("current");
+});
+
+historiquebtn.click(function() {
+	$("#nav-accueil span").text("");
+	accueilbtn.removeClass("current");
+
+	$("#nav-exporter span").text("");
+	exporterbtn.removeClass("current");
+
+	$("#nav-historique span").text("Historique");
+	historiquebtn.addClass("current");
+});
+
+exporterbtn.click(function() {
+	$("#nav-accueil span").text("");
+	accueilbtn.removeClass("current");
+
+	$("#nav-historique span").text("");
+	historiquebtn.removeClass("current");
+
+	$("#nav-exporter span").text("Exporter");
+	exporterbtn.addClass("current");
+});
