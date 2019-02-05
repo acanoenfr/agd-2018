f3677336c23de31d75052e3a7053632d65c1d914
Maysoon <maysoondoucanse@gmail.com>
2019-02-05 10:54:03 +0100

    Auto stash before merge of "master" and "origin/master"

--- www/js/exportCSV.js
+++ www/js/exportCSV.js
@@ -1,47 +1,43 @@
 var donnees=[];
 let db = window.openDatabase("Events00000000", "1.0", "All Deadlines", 2000000);
-db.transaction(function (tx) {
-    tx.executeSql('Select * from events', [], function (tx, result) {
-        for(let i=0; i<result.rows.length; i++){                      
-            donnees.push(result.rows[i]);
-            donnees.sort(function(a,b){
-                if(a.start<b.start){
-                    return -1; }
-                if(a.start>b.start){
-                    return 1; }
-                return 0;});
-             }                                  
+
+    db.transaction(function (tx) {
+        tx.executeSql('Select * from events', [], function (tx, result) {
+            for(let i=0; i<result.rows.length; i++){                      
+                donnees.push(result.rows[i]);
+                donnees.sort(function(a,b){
+                    if(a.start<b.start){
+                        return -1;
+                    }
+                    if(a.start>b.start){
+                        return 1;
+                    }
+                    return 0;
+                })
+            }                               
+        });
+    }, function (error) {
+        console.log('Transaction ERROR: ' + error.message);
+    }, function () {   
+        console.log(donnees)
     });
-}, function (error) {
-    console.log('Transaction ERROR: ' + error.message);
-}, function () {   
-    console.log(donnees);   
-});
 
 
-var stockData = [  
-        {
-            Symbol: "AAPL",
-            Company: "Apple Inc.",
-            Price: 132.54
-        },
-        {
-            Symbol: "INTC",
-            Company: "Intel Corporation",
-            Price: 33.45
-        },
-        {
-            Symbol: "GOOG",
-            Company: "Google Inc",
-            Price: 554.52
-        },
-    ];
-function convertArrayOfObjectsToCSV(args) {  
-        var result, ctr, keys, columnDelimiter, lineDelimiter, data;
 
-        data = args.data || null;
-        if (data == null || !data.length) {
-            return null;
+
+
+
+
+
+
+// fichier csv
+var stockData=[ ];
+function convertArrayOfObjectsToCSV(args){  
+        var result,ctr,keys,columnDelimiter,lineDelimiter,data;
+
+        data=args.data||null;
+        if (data==null||!data.length){
+            return null
         }
 
         columnDelimiter = args.columnDelimiter || ',   ';
@@ -54,10 +50,10 @@ function convertArrayOfObjectsToCSV(args) {
         result += lineDelimiter;
 /*elle parcourt l'un des obj pour creer une ligne d'en-tete, 
 suivie d'une nouvelle ligne*/
-        data.forEach(function(item) {
+        data.forEach(function(item){
             ctr = 0;
   /*ensuite je parcours chaque objet avec les valeurs de chaque propriete*/
-            keys.forEach(function(key) {
+            keys.forEach(function(key){
                 if (ctr > 0) result += columnDelimiter;
 
                 result += item[key];
@@ -74,8 +70,8 @@ suivie d'une nouvelle ligne*/
 le data et le retourne en fichier csv pour telecharger
 */
 
-	function downloadCSV(args) {
-		var data, filename, link; 
+	function downloadCSV(args){
+		var data,filename,link; 
 		var csv = convertArrayOfObjectsToCSV({
 			data: stockData
 		}); 
